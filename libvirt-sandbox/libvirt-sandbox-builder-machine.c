89a1e74b3d8e06db13568b7177320c2b3968cad1
Daniel P. Berrange <berrange@redhat.com>
2011-11-24 17:25:25 +0000

    Fix encoding of command argv to include length prefix

--- libvirt-sandbox/libvirt-sandbox-builder-machine.c
+++ libvirt-sandbox/libvirt-sandbox-builder-machine.c
@@ -151,8 +151,10 @@ static gchar *gvir_sandbox_builder_machine_cmdline(GVirSandboxConfig *config)
     gchar **argv, **tmp;
     gchar *cmdrawstr;
     gchar *cmdb64str;
-    size_t len = 0, offset;
+    gsize len = 0, offset = 0;
     gchar *ret;
+    gchar *lenstr;
+    gsize prefix = 0;
 
     argv = gvir_sandbox_config_get_command(config);
 
@@ -162,9 +164,15 @@ static gchar *gvir_sandbox_builder_machine_cmdline(GVirSandboxConfig *config)
         tmp++;
     }
 
-    cmdrawstr = g_new0(gchar, len);
+    lenstr = g_strdup_printf("%zu", len);
+    prefix = strlen(lenstr);
+    cmdrawstr = g_new0(gchar, prefix + 1 + len);
+
+    memcpy(cmdrawstr, lenstr, prefix);
+    cmdrawstr[prefix] = ':';
+    offset += prefix + 1;
+
     tmp = argv;
-    offset = 0;
     while (*tmp) {
         size_t tlen = strlen(*tmp);
         memcpy(cmdrawstr + offset, *tmp, tlen + 1);
@@ -172,7 +180,7 @@ static gchar *gvir_sandbox_builder_machine_cmdline(GVirSandboxConfig *config)
         tmp++;
     }
 
-    cmdb64str = g_base64_encode((guchar*)cmdrawstr, len);
+    cmdb64str = g_base64_encode((guchar*)cmdrawstr, prefix + 1 + len);
     g_free(cmdrawstr);
 
     /* First sandbox specific args */
