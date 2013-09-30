b14ce17bb357d5b7f99562c91dafb521f6985b40
Daniel P. Berrange <berrange@redhat.com>
2013-09-30 15:13:06 +0100

    Always set earlyprintk kernel arg
    If the initrd fails and prints to stderr, this goes to /dev/null
    unless earlyprintk is enabled. We always want to see initrd
    errors, so we should always have earlyprintk set.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- libvirt-sandbox/libvirt-sandbox-builder-machine.c
+++ libvirt-sandbox/libvirt-sandbox-builder-machine.c
@@ -199,10 +199,10 @@ static gchar *gvir_sandbox_builder_machine_cmdline(GVirSandboxConfig *config G_G
     gchar *tmp;
 
     /* Now kernel args */
-    g_string_append(str, " console=ttyS0");
+    g_string_append(str, " console=ttyS0 earlyprintk=ttyS0");
     if (getenv("LIBVIRT_SANDBOX_DEBUG") &&
         g_str_equal(getenv("LIBVIRT_SANDBOX_DEBUG"), "2"))
-        g_string_append(str, " debug loglevel=10 earlyprintk=ttyS0");
+        g_string_append(str, " debug loglevel=10");
     else
         g_string_append(str, " quiet loglevel=0");
 
