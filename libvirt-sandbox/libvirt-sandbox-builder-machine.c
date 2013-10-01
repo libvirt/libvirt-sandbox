b20c8945af5a5ce248c2acd895be2812965fd6bd
Daniel P. Berrange <berrange@redhat.com>
2013-10-01 13:54:10 +0100

    Force 9p version to version=9p2000.u
    With 9p version=9p2000.L, we tickle two bugs in QEMU's code.
    
    One breaks most calls with ENODEV on FS_IOC_GETVERSION ioctls.
    
    The other breaks xattr checks due to inverted errno.
    
    In addition with 9p2000.L we see extra permission checks
    on dirs, which prevents the guest from over-mounting dirs
    like /root that are restricted on the user running QEMU.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    (cherry picked from commit f4087aa0e993f59772d3bda53038888af7f43b5e)

--- libvirt-sandbox/libvirt-sandbox-builder-machine.c
+++ libvirt-sandbox/libvirt-sandbox-builder-machine.c
@@ -274,7 +274,7 @@ static gboolean gvir_sandbox_builder_machine_write_mount_cfg(GVirSandboxConfig *
         if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_BIND(mconfig)) {
             source = g_strdup_printf("sandbox:mount%zu", nHostBind++);
             fstype = "9p";
-            options = g_strdup("trans=virtio");
+            options = g_strdup("trans=virtio,version=9p2000.u");
         } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_IMAGE(mconfig)) {
             source = g_strdup_printf("vd%c", (char)('a' + nHostImage++));
             fstype = "ext3";
