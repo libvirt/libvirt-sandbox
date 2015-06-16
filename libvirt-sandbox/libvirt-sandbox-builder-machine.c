libvirt/libvirt-sandbox

commit d74b4350ac7d42b9618f82839dd0829984168181

Preserve user-specified ordering when mounting filesystems
Since there is now a single API for configuring mounts it
is now possible to maintain this order when mounting the
filesystems inside the sandbox. Remove the bind mount code
from init-common and let libvirt handled LXC and make the
init-qemu binary handle QEMU/KVM

libvirt-sandbox/libvirt-sandbox-builder-machine.c
@@ -208,14 +208,12 @@ static gchar *gvir_sandbox_builder_machine_cmdline(GVirSandboxConfig *config G_G
 }
 
 
-static gboolean gvir_sandbox_builder_machine_write_mount_cfg(GList *mounts,
-                                                             gboolean isDisk,
-                                                             const gchar *filename,
+static gboolean gvir_sandbox_builder_machine_write_mount_cfg(GVirSandboxConfig *config,
                                                              const gchar *configdir,
                                                              GVirSandboxCleaner *cleaner,
                                                              GError **error)
 {
-    gchar *mntfile = g_strdup_printf("%s/%s", configdir, filename);
+    gchar *mntfile = g_strdup_printf("%s/mounts.cfg", configdir);
     GFile *file = g_file_new_for_path(mntfile);
     GFileOutputStream *fos = g_file_replace(file,
                                             NULL,
@@ -224,32 +222,53 @@ static gboolean gvir_sandbox_builder_machine_write_mount_cfg(GList *mounts,
                                             NULL,
                                             error);
     gboolean ret = FALSE;
+    GList *mounts = gvir_sandbox_config_get_mounts(config);
     GList *tmp = NULL;
-    int i;
+    size_t nHostBind = 0;
+    size_t nHostImage = 0;
 
     if (!fos)
         goto cleanup;
 
-    i = 0;
     tmp = mounts;
     while (tmp) {
-        GVirSandboxConfigMount *mount = tmp->data;
-        gchar *key;
+        GVirSandboxConfigMount *mconfig = GVIR_SANDBOX_CONFIG_MOUNT(tmp->data);
+        const gchar *fstype;
+        gchar *source;
+        const gchar *options;
+        const gchar *target;
+        gchar *line;
 
-        if (isDisk)
-            key = g_strdup_printf("/dev/vd%c=%s", 'a' + i,
-                                  gvir_sandbox_config_mount_get_target(mount));
-        else
-            key = g_strdup_printf("sandbox:mount%u=%s\n", i,
-                                  gvir_sandbox_config_mount_get_target(mount));
+        if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_BIND(mconfig)) {
+            source = g_strdup_printf("sandbox:mount%zu", nHostBind++);
+            fstype = "9p";
+            options = "trans=virtio";
+        } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_IMAGE(mconfig)) {
+            source = g_strdup_printf("vd%c", (char)('a' + nHostImage++));
+            fstype = "ext3";
+            options = "";
+        } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_GUEST_BIND(mconfig)) {
+            GVirSandboxConfigMountFile *mfile = GVIR_SANDBOX_CONFIG_MOUNT_FILE(mconfig);
+            source = g_strdup(gvir_sandbox_config_mount_file_get_source(mfile));
+            fstype = "";
+            options = "";
+        } else {
+            g_assert_not_reached();
+        }
+        target = gvir_sandbox_config_mount_get_target(mconfig);
 
-        if (!g_output_stream_write_all(G_OUTPUT_STREAM(fos), key, strlen(key),
+        line = g_strdup_printf("%s\t%s\t%s\t%s\n",
+                               source, target, fstype, options);
+        g_free(source);
+
+        if (!g_output_stream_write_all(G_OUTPUT_STREAM(fos),
+                                       line, strlen(line),
                                        NULL, NULL, error)) {
-            g_free(key);
+            g_free(line);
             goto cleanup;
         }
+        g_free(line);
 
-        i++;
         tmp = tmp->next;
     }
 
@@ -280,39 +299,17 @@ static gboolean gvir_sandbox_builder_machine_construct_domain(GVirSandboxBuilder
                                                               GVirConfigDomain *domain,
                                                               GError **error)
 {
-    GList *hostBind = gvir_sandbox_config_get_mounts_with_type(config,
-                                                               GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_BIND);
-    GList *hostImage = gvir_sandbox_config_get_mounts_with_type(config,
-                                                                GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_IMAGE);
-    gboolean ret = FALSE;
-
-    if (!gvir_sandbox_builder_machine_write_mount_cfg(hostBind,
-                                                      FALSE,
-                                                      "filesys.cfg",
+    if (!gvir_sandbox_builder_machine_write_mount_cfg(config,
                                                       configdir,
                                                       cleaner,
                                                       error))
-        goto cleanup;
-
-    if (!gvir_sandbox_builder_machine_write_mount_cfg(hostImage,
-                                                      TRUE,
-                                                      "images.cfg",
-                                                      configdir,
-                                                      cleaner,
-                                                      error))
-        goto cleanup;
+        return FALSE;
 
     if (!GVIR_SANDBOX_BUILDER_CLASS(gvir_sandbox_builder_machine_parent_class)->
         construct_domain(builder, config, configdir, cleaner, domain, error))
-        goto cleanup;
+        return FALSE;
 
-    ret = TRUE;
-cleanup:
-    g_list_foreach(hostBind, (GFunc)g_object_unref, NULL);
-    g_list_foreach(hostImage, (GFunc)g_object_unref, NULL);
-    g_list_free(hostBind);
-    g_list_free(hostImage);
-    return ret;
+    return TRUE;
 }
 
 static gboolean gvir_sandbox_builder_machine_construct_basic(GVirSandboxBuilder *builder,
@@ -427,7 +424,8 @@ static gboolean gvir_sandbox_builder_machine_construct_devices(GVirSandboxBuilde
     GVirConfigDomainSerial *ser;
     GVirConfigDomainChardevSourcePty *src;
     GList *tmp = NULL, *mounts = NULL, *networks = NULL;
-    int i;
+    size_t nHostBind = 0;
+    size_t nHostImage = 0;
 
     if (!GVIR_SANDBOX_BUILDER_CLASS(gvir_sandbox_builder_machine_parent_class)->
         construct_devices(builder, config, configdir, cleaner, domain, error))
@@ -458,57 +456,46 @@ static gboolean gvir_sandbox_builder_machine_construct_devices(GVirSandboxBuilde
     g_object_unref(fs);
 
 
-
-    tmp = mounts = gvir_sandbox_config_get_mounts_with_type(config,
-                                                            GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_BIND);
-    i = 0;
+    tmp = mounts = gvir_sandbox_config_get_mounts(config);
     while (tmp) {
-        GVirSandboxConfigMountFile *mconfig = tmp->data;
-        gchar *target = g_strdup_printf("sandbox:mount%d", i);
+        GVirSandboxConfigMount *mconfig = GVIR_SANDBOX_CONFIG_MOUNT(tmp->data);
 
-        fs = gvir_config_domain_filesys_new();
-        gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_MOUNT);
-        gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_MAPPED);
-        gvir_config_domain_filesys_set_source(fs,
-                                              gvir_sandbox_config_mount_file_get_source(mconfig));
-        gvir_config_domain_filesys_set_target(fs, target);
+        if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_BIND(mconfig)) {
+            GVirSandboxConfigMountFile *mfile = GVIR_SANDBOX_CONFIG_MOUNT_FILE(mconfig);
 
-        gvir_config_domain_add_device(domain,
-                                      GVIR_CONFIG_DOMAIN_DEVICE(fs));
-        g_object_unref(fs);
-        g_free(target);
+            gchar *target = g_strdup_printf("sandbox:mount%zu", nHostBind++);
 
+            fs = gvir_config_domain_filesys_new();
+            gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_MOUNT);
+            gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_MAPPED);
+            gvir_config_domain_filesys_set_source(fs,
+                                                  gvir_sandbox_config_mount_file_get_source(mfile));
+            gvir_config_domain_filesys_set_target(fs, target);
+
+            gvir_config_domain_add_device(domain,
+                                          GVIR_CONFIG_DOMAIN_DEVICE(fs));
+            g_object_unref(fs);
+            g_free(target);
+
+        } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_IMAGE(mconfig)) {
+            GVirSandboxConfigMountFile *mfile = GVIR_SANDBOX_CONFIG_MOUNT_FILE(mconfig);
+            gchar *target = g_strdup_printf("vd%c", (char)('a' + nHostImage++));
+
+            disk = gvir_config_domain_disk_new();
+            gvir_config_domain_disk_set_type(disk, GVIR_CONFIG_DOMAIN_DISK_FILE);
+            gvir_config_domain_disk_set_source(disk,
+                                               gvir_sandbox_config_mount_file_get_source(mfile));
+            gvir_config_domain_disk_set_target_dev(disk, target);
+
+            gvir_config_domain_add_device(domain,
+                                          GVIR_CONFIG_DOMAIN_DEVICE(disk));
+            g_object_unref(disk);
+            g_free(target);
+        }
         tmp = tmp->next;
-        i++;
     }
     g_list_foreach(mounts, (GFunc)g_object_unref, NULL);
     g_list_free(mounts);
-
-
-    tmp = mounts = gvir_sandbox_config_get_mounts_with_type(config,
-                                                            GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_IMAGE);
-    i = 0;
-    while (tmp) {
-        GVirSandboxConfigMountFile *mconfig = tmp->data;
-        gchar *target = g_strdup_printf("vd%c", 'a' + i);
-
-        disk = gvir_config_domain_disk_new();
-        gvir_config_domain_disk_set_type(disk, GVIR_CONFIG_DOMAIN_DISK_FILE);
-        gvir_config_domain_disk_set_source(disk,
-                                           gvir_sandbox_config_mount_file_get_source(mconfig));
-        gvir_config_domain_disk_set_target_dev(disk, target);
-
-        gvir_config_domain_add_device(domain,
-                                      GVIR_CONFIG_DOMAIN_DEVICE(disk));
-        g_object_unref(disk);
-        g_free(target);
-
-        tmp = tmp->next;
-        i++;
-    }
-    g_list_foreach(mounts, (GFunc)g_object_unref, NULL);
-    g_list_free(mounts);
-
 
     tmp = networks = gvir_sandbox_config_get_networks(config);
     while (tmp) {

configure.ac
@@ -12,8 +12,8 @@ AM_SILENT_RULES([yes])
 
 GIO_UNIX_REQUIRED=2.28.0
 GOBJECT_REQUIRED=2.32.0
-LIBVIRT_GLIB_REQUIRED=0.0.4
-LIBVIRT_GOBJECT_REQUIRED=0.0.7
+LIBVIRT_GLIB_REQUIRED=0.0.9
+LIBVIRT_GOBJECT_REQUIRED=0.0.9
 GOBJECT_INTROSPECTION_REQUIRED=0.10.8
 
 LIBVIRT_SANDBOX_MAJOR_VERSION=`echo $VERSION | awk -F. '{print $1}'`

libvirt-sandbox.spec.in
@@ -20,7 +20,7 @@ License: LGPLv2+
 URL: http://libvirt.org/
 Source0: http://libvirt.org/%{name}/%{name}-%{version}.tar.gz
 BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
-BuildRequires: libvirt-gobject-devel >= 0.0.7
+BuildRequires: libvirt-gobject-devel >= 0.0.9
 %if %{with_introspection}
 BuildRequires: gobject-introspection-devel
 %endif
@@ -31,7 +31,7 @@ Requires: glib2-devel >= 2.32.0
 Group: Development/Libraries
 Summary: libvirt application sandbox framework libraries
 # So we get the full libvirtd daemon, not just client libs
-Requires: libvirt >= 0.9.11
+Requires: libvirt >= 0.9.12
 
 %package devel
 Group: Development/Libraries

libvirt-sandbox/libvirt-sandbox-builder-container.c
@@ -243,45 +243,54 @@ static gboolean gvir_sandbox_builder_container_construct_devices(GVirSandboxBuil
 
 
 
-    tmp = mounts = gvir_sandbox_config_get_mounts_with_type(config,
-                                                            GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_BIND);
+    tmp = mounts = gvir_sandbox_config_get_mounts(config);
     while (tmp) {
-        GVirSandboxConfigMountFile *mconfig = tmp->data;
+        GVirSandboxConfigMount *mconfig = GVIR_SANDBOX_CONFIG_MOUNT(tmp->data);
 
-        fs = gvir_config_domain_filesys_new();
-        gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_MOUNT);
-        gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_PASSTHROUGH);
-        gvir_config_domain_filesys_set_source(fs,
-                                              gvir_sandbox_config_mount_file_get_source(mconfig));
-        gvir_config_domain_filesys_set_target(fs,
-                                              gvir_sandbox_config_mount_get_target(GVIR_SANDBOX_CONFIG_MOUNT(mconfig)));
+        if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_BIND(mconfig)) {
+            GVirSandboxConfigMountFile *mfile = GVIR_SANDBOX_CONFIG_MOUNT_FILE(mconfig);
 
-        gvir_config_domain_add_device(domain,
-                                      GVIR_CONFIG_DOMAIN_DEVICE(fs));
-        g_object_unref(fs);
+            fs = gvir_config_domain_filesys_new();
+            gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_MOUNT);
+            gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_PASSTHROUGH);
+            gvir_config_domain_filesys_set_source(fs,
+                                                  gvir_sandbox_config_mount_file_get_source(mfile));
+            gvir_config_domain_filesys_set_target(fs,
+                                                  gvir_sandbox_config_mount_get_target(mconfig));
 
-        tmp = tmp->next;
-    }
-    g_list_foreach(mounts, (GFunc)g_object_unref, NULL);
-    g_list_free(mounts);
+            gvir_config_domain_add_device(domain,
+                                          GVIR_CONFIG_DOMAIN_DEVICE(fs));
+            g_object_unref(fs);
+        } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_IMAGE(mconfig)) {
+            GVirSandboxConfigMountFile *mfile = GVIR_SANDBOX_CONFIG_MOUNT_FILE(mconfig);
 
+            fs = gvir_config_domain_filesys_new();
+            gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_FILE);
+            gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_PASSTHROUGH);
+            gvir_config_domain_filesys_set_source(fs,
+                                                  gvir_sandbox_config_mount_file_get_source(mfile));
+            gvir_config_domain_filesys_set_target(fs,
+                                                  gvir_sandbox_config_mount_get_target(mconfig));
 
-    tmp = mounts = gvir_sandbox_config_get_mounts_with_type(config,
-                                                            GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_IMAGE);
-    while (tmp) {
-        GVirSandboxConfigMountFile *mconfig = tmp->data;
+            gvir_config_domain_add_device(domain,
+                                          GVIR_CONFIG_DOMAIN_DEVICE(fs));
+            g_object_unref(fs);
+        } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_GUEST_BIND(mconfig)) {
+            GVirSandboxConfigMountFile *mfile = GVIR_SANDBOX_CONFIG_MOUNT_FILE(mconfig);
 
-        fs = gvir_config_domain_filesys_new();
-        gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_FILE);
-        gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_PASSTHROUGH);
-        gvir_config_domain_filesys_set_source(fs,
-                                              gvir_sandbox_config_mount_file_get_source(mconfig));
-        gvir_config_domain_filesys_set_target(fs,
-                                              gvir_sandbox_config_mount_get_target(GVIR_SANDBOX_CONFIG_MOUNT(mconfig)));
+            fs = gvir_config_domain_filesys_new();
+            gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_BIND);
+            gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_PASSTHROUGH);
+            gvir_config_domain_filesys_set_source(fs,
+                                                  gvir_sandbox_config_mount_file_get_source(mfile));
+            gvir_config_domain_filesys_set_target(fs,
+                                                  gvir_sandbox_config_mount_get_target(mconfig));
 
-        gvir_config_domain_add_device(domain,
-                                      GVIR_CONFIG_DOMAIN_DEVICE(fs));
-        g_object_unref(fs);
+            gvir_config_domain_add_device(domain,
+                                          GVIR_CONFIG_DOMAIN_DEVICE(fs));
+            g_object_unref(fs);
+        }
+
 
         tmp = tmp->next;
     }

libvirt-sandbox/libvirt-sandbox-config.c
@@ -1084,11 +1084,11 @@ gboolean gvir_sandbox_config_add_mount_strv(GVirSandboxConfig *config,
                         "No mount type prefix on %s", mounts[i]);
             return FALSE;
         }
-        if (strncmp(tmp, "host-bind", (tmp - mounts[i])) == 0) {
+        if (strncmp(mounts[i], "host-bind", (tmp - mounts[i])) == 0) {
             type = GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_BIND;
-        } else if (strncmp(tmp, "host-image", (tmp - mounts[i])) == 0) {
+        } else if (strncmp(mounts[i], "host-image", (tmp - mounts[i])) == 0) {
             type = GVIR_SANDBOX_TYPE_CONFIG_MOUNT_HOST_IMAGE;
-        } else if (strncmp(tmp, "guest-bind", (tmp - mounts[i])) == 0) {
+        } else if (strncmp(mounts[i], "guest-bind", (tmp - mounts[i])) == 0) {
             type = GVIR_SANDBOX_TYPE_CONFIG_MOUNT_GUEST_BIND;
         } else {
             g_set_error(error, GVIR_SANDBOX_CONFIG_ERROR, 0,
@@ -1389,7 +1389,12 @@ static GVirSandboxConfigMount *gvir_sandbox_config_load_config_mount(GKeyFile *f
         goto error;
     }
 
-    if ((mountType = g_type_from_name(type)) == G_TYPE_NONE) {
+    /* Force initialization of types we care about */
+    gvir_sandbox_config_mount_host_bind_get_type();
+    gvir_sandbox_config_mount_host_image_get_type();
+    gvir_sandbox_config_mount_guest_bind_get_type();
+
+    if ((mountType = g_type_from_name(type)) == 0) {
         g_set_error(error, GVIR_SANDBOX_CONFIG_ERROR, 0,
                     "Unknown mount type %s in config file", type);
         goto error;

libvirt-sandbox/libvirt-sandbox-init-common.c
@@ -53,14 +53,6 @@ static int sigwrite;
 
 #define ATTR_UNUSED __attribute__((__unused__))
 
-#define GVIR_SANDBOX_INIT_COMMON_ERROR gvir_sandbox_init_common_error_quark()
-
-static GQuark
-gvir_sandbox_init_common_error_quark(void)
-{
-    return g_quark_from_static_string("gvir-sandbox-init-common");
-}
-
 static void sig_child(int sig ATTR_UNUSED)
 {
     char ignore = '1';
@@ -275,46 +267,6 @@ cleanup:
     g_free(devname);
     g_list_foreach(nets, (GFunc)g_object_unref, NULL);
     g_list_free(nets);
-    return ret;
-}
-
-
-static gboolean setup_bind_mount(GVirSandboxConfigMountFile *config, GError **error)
-{
-    const gchar *src = gvir_sandbox_config_mount_file_get_source(config);
-    const gchar *tgt = gvir_sandbox_config_mount_get_target(GVIR_SANDBOX_CONFIG_MOUNT(config));
-
-    if (mount(src, tgt, NULL, MS_BIND, NULL) < 0) {
-        g_set_error(error, GVIR_SANDBOX_INIT_COMMON_ERROR, 0,
-                    "Cannot bind %s to %s", src, tgt);
-        return FALSE;
-    }
-
-    return TRUE;
-}
-
-
-static gboolean setup_bind_mounts(GVirSandboxConfig *config, GError **error)
-{
-    GList *mounts, *tmp;
-    gboolean ret = FALSE;
-
-    tmp = mounts = gvir_sandbox_config_get_mounts_with_type(config,
-                                                            GVIR_SANDBOX_TYPE_CONFIG_MOUNT_GUEST_BIND);
-    while (tmp) {
-        GVirSandboxConfigMountFile *mntconfig = tmp->data;
-
-        if (!setup_bind_mount(mntconfig, error))
-            goto cleanup;
-
-        tmp = tmp->next;
-    }
-
-    ret = TRUE;
-
-cleanup:
-    g_list_foreach(mounts, (GFunc)g_object_unref, NULL);
-    g_list_free(mounts);
     return ret;
 }
 
@@ -1222,9 +1174,6 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
 
     if (!setup_network(config, &error))
-        goto error;
-
-    if (!setup_bind_mounts(config, &error))
         goto error;
 
     if (change_user(gvir_sandbox_config_get_username(config),

libvirt-sandbox/libvirt-sandbox-init-qemu.c
@@ -106,7 +106,6 @@ mount_mkdir(const char *dir, int mode)
     }
 }
 
-#if 0
 static void
 mount_mkfile(const char *file, int mode)
 {
@@ -115,17 +114,17 @@ mount_mkfile(const char *file, int mode)
     mount_mkparent(file, 0755);
 
     if (debug)
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: %s (mode=%o)\n", __func__, file, mode);
+        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: %s (mode=%o)\n",
+                __func__, file, mode);
 
     if ((fd = open(file, O_CREAT|O_WRONLY, mode)) < 0 &&
         errno != EEXIST) {
         fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot open file %s: %s\n",
-                __func__, dir, strerror(errno));
+                    __func__, file, strerror(errno));
         exit_poweroff();
     }
     close(fd);
 }
-#endif
 
 static void
 mount_other_opts(const char *dst, const char *type, const char *opts, int mode)
@@ -173,59 +172,8 @@ mount_9pfs(const char *src, const char *dst, int mode, int readonly)
     }
 }
 
-static void
-mount_ext(const char *src, const char *dst, int mode, int readonly)
-{
-    int flags = 0;
-    if (debug)
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: %s -> %s (%d)\n", __func__, src, dst, readonly);
-
-    mount_mkdir(dst, mode);
-
-    if (readonly)
-        flags |= MS_RDONLY;
-
-    if (mount(src, dst, "ext4", flags, "") < 0) {
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot mount %s on %s (ext4): %s\n",
-                __func__, src, dst, strerror(errno));
-        exit_poweroff();
-    }
-}
-
-#if 0
-static void
-bind_dir(const char *src, const char *dst, int mode)
-{
-    if (debug)
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: %s -> %s\n", __func__, src, dst);
-
-    mount_mkdir(dst, mode);
-
-    if (mount(src, dst, "", MS_BIND, "") < 0) {
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot bind %s to %s: %s\n",
-                __func__, src, dst, strerror(errno));
-        exit_poweroff();
-    }
-}
-
-static void
-bind_file(const char *src, const char *dst, int mode)
-{
-    if (debug)
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: %s -> %s\n", __func__, src, dst);
-
-    mount_mkfile(dst, mode);
-
-    if (mount(src, dst, "", MS_BIND, "") < 0) {
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot bind %s to %s: %s\n",
-                __func__, src, dst, strerror(errno));
-        exit_poweroff();
-    }
-}
-#endif
-
 static int virtioblk_major = 0;
-static int
+static void
 create_virtioblk_device(const char *dev)
 {
     int minor;
@@ -235,7 +183,7 @@ create_virtioblk_device(const char *dev)
         if (!fp) {
             fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot read /proc/devices: %s\n",
                     __func__, strerror(errno));
-            return -1;
+            exit_poweroff();
         }
         while (fgets(line2, sizeof line2, fp)) {
             if (strstr(line2, "virtblk")) {
@@ -245,7 +193,7 @@ create_virtioblk_device(const char *dev)
                     fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot extract device major from '%s'\n",
                             __func__, line2);
                     fclose(fp);
-                    return -1;
+                    exit_poweroff();
                 }
                 virtioblk_major = l;
                 break;
@@ -256,7 +204,7 @@ create_virtioblk_device(const char *dev)
         if (virtioblk_major == 0) {
             fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot find virtioblk device major in /proc/devices\n",
                     __func__);
-            return -1;
+            exit_poweroff();
         }
     }
 
@@ -265,9 +213,8 @@ create_virtioblk_device(const char *dev)
     if (mknod(dev, S_IFBLK |0700, makedev(virtioblk_major, minor)) < 0) {
         fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot make dev '%s' '%llu': %s\n",
                 __func__, dev, makedev(virtioblk_major, minor), strerror(errno));
-        return -1;
+        exit_poweroff();
     }
-    return 0;
 }
 
 int
@@ -394,48 +341,59 @@ main(int argc ATTR_UNUSED, char **argv ATTR_UNUSED)
     if (debug)
         fprintf(stderr, "libvirt-sandbox-init-qemu: %s: setting up filesystem mounts\n",
                 __func__);
-    fp = fopen(SANDBOXCONFIGDIR "/filesys.cfg", "r");
+    fp = fopen(SANDBOXCONFIGDIR "/mounts.cfg", "r");
     if (fp == NULL) {
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot open " SANDBOXCONFIGDIR "/filesys.cfg: %s\n",
+        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot open " SANDBOXCONFIGDIR "/mounts.cfg: %s\n",
                 __func__, strerror(errno));
         exit_poweroff();
     }
     while (fgets(line, sizeof line, fp)) {
-        char *target = strchr(line, '=');
+        char *source = line;
+        char *target = strchr(source, '\t');
         *target = '\0';
         target++;
-        size_t n = strlen(target);
-        if (n > 0 && target[n-1] == '\n')
-            target[--n] = '\0';
+        char *type = strchr(target, '\t');
+        *type = '\0';
+        type++;
+        char *opts = strchr(type, '\t');
+        *opts = '\0';
+        opts++;
+        char *tmp = strchr(opts, '\n');
+        *tmp = '\0';
+        int flags = 0;
 
-        mount_9pfs(line, target, 0755, 0);
-    }
-    fclose(fp);
+        if (debug)
+            fprintf(stderr, "libvirt-sandbox-init-qemu: %s: %s -> %s (%s, %s)\n",
+                    __func__, source, target, type, opts);
 
+        if (strncmp(source, "/dev/vd", 7) == 0)
+            create_virtioblk_device(source);
 
-    if (debug)
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: setting up image mounts\n",
-                __func__);
-    fp = fopen(SANDBOXCONFIGDIR "/images.cfg", "r");
-    if (fp == NULL) {
-        fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot open " SANDBOXCONFIGDIR "/images.cfg: %s\n",
-                __func__, strerror(errno));
-        exit_poweroff();
-    }
-    while (fgets(line, sizeof line, fp)) {
-        char *target = strchr(line, '=');
-        *target = '\0';
-        target++;
-        size_t n = strlen(target);
-        if (n > 0 && target[n-1] == '\n')
-            target[--n] = '\0';
+        if (strcmp(type, "") == 0) {
+            struct stat st;
+            type = NULL;
+            flags |= MS_BIND;
+            if (stat(source, &st) < 0) {
+                fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot read mount source %s: %s\n",
+                        __func__, source, strerror(errno));
+                exit_poweroff();
+            }
+            if (S_ISDIR(st.st_mode))
+                mount_mkdir(target, 755);
+            else
+                mount_mkfile(target, 644);
+        } else {
+            mount_mkdir(target, 0755);
+        }
 
-        if (create_virtioblk_device(line) < 0)
+        if (mount(source, target, type, flags, opts) < 0) {
+            fprintf(stderr, "libvirt-sandbox-init-qemu: %s: cannot mount %s on %s (%s, %s): %s\n",
+                    __func__, source, target, type, opts, strerror(errno));
             exit_poweroff();
-
-        mount_ext(line, target, 0755, 0);
+        }
     }
     fclose(fp);
+
 
     if (debug)
         fprintf(stderr, "libvirt-sandbox-init-qemu: %s: preparing to launch common init\n",

libvirt-sandbox/tests/test-config.c
@@ -52,9 +52,9 @@ int main(int argc, char **argv)
     gchar *f2 = NULL;
     int ret = EXIT_FAILURE;
     const gchar *mounts[] = {
-        "/var/run/hell=/tmp/home",
-        "/etc=/tmp/home",
-        "/tmp=",
+        "host-bind:/var/run/hell=/tmp/home",
+        "host-image:/etc=/tmp/home",
+        "host-bind:/tmp=",
         NULL
     };
     const gchar *includes[] = {
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
     gvir_sandbox_config_set_username(cfg1, "superdevil");
     gvir_sandbox_config_set_homedir(cfg1, "/var/run/hell");
 
-    if (!gvir_sandbox_config_add_host_bind_mount_strv(cfg1, (gchar**)mounts, &err))
+    if (!gvir_sandbox_config_add_mount_strv(cfg1, (gchar**)mounts, &err))
         goto cleanup;
 
     if (!gvir_sandbox_config_add_host_include_strv(cfg1, (gchar**)includes, &err))

commit c0059d79c5290a47997ff0904de88e8b16730278

Write /dev/vd* instead of vd* in mounts.cfg
Fixes a regression introduced by d74b4350: the init-qemu tool expects
/dev/vd* sources to create the block device, while we were just having
vd*. Write again /dev/vd* to mounts.cfg.

libvirt-sandbox/libvirt-sandbox-builder-machine.c
@@ -303,7 +303,7 @@ static gboolean gvir_sandbox_builder_machine_write_mount_cfg(GVirSandboxConfig *
             fstype = "9p";
             options = g_strdup("trans=virtio,version=9p2000.u");
         } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_HOST_IMAGE(mconfig)) {
-            source = g_strdup_printf("vd%c", (char)('a' + nHostImage++));
+            source = g_strdup_printf("/dev/vd%c", (char)('a' + nHostImage++));
             fstype = "ext3";
             options = g_strdup("");
         } else if (GVIR_SANDBOX_IS_CONFIG_MOUNT_GUEST_BIND(mconfig)) {