2f9f121594f24123b42d087110fb67c466fdbf50
Daniel P. Berrange <berrange@redhat.com>
2015-06-30 16:48:04 +0100

    Use passthrough mode for all file systems
    The QEMU 'mapped' access mode has strange semantics wrt symlinks.
    It does not use symlinks in the filesystem, instead creating
    regular files and setting an xattr to say they are a symlink.
    This makes it impossible to use generic pre-built filesystem
    trees.

--- libvirt-sandbox/libvirt-sandbox-builder-machine.c
+++ libvirt-sandbox/libvirt-sandbox-builder-machine.c
@@ -582,7 +582,7 @@ static gboolean gvir_sandbox_builder_machine_construct_devices(GVirSandboxBuilde
 
             fs = gvir_config_domain_filesys_new();
             gvir_config_domain_filesys_set_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_MOUNT);
-            gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_MAPPED);
+            gvir_config_domain_filesys_set_access_type(fs, GVIR_CONFIG_DOMAIN_FILESYS_ACCESS_PASSTHROUGH);
             gvir_config_domain_filesys_set_source(fs,
                                                   gvir_sandbox_config_mount_file_get_source(mfile));
             gvir_config_domain_filesys_set_target(fs, target);
