e58acc3d347c664bac53c51425bc95b616feaf6a
Andrea Bolognani <abologna@redhat.com>
2018-05-07 16:07:04 +0200

    prepare-release: Drop references to MinGW builds
    The MinGW spec file was never included in the git repository
    to begin with.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -46,26 +46,3 @@ if [ -f /usr/bin/rpmbuild ]; then
 fi
 
 exit 0
-if [ -x /usr/bin/i686-pc-mingw32-gcc ]; then
-  make distclean
-
-  PKG_CONFIG_PATH="$AUTOBUILD_INSTALL_ROOT/i686-pc-mingw32/sys-root/mingw/lib/pkgconfig" \
-  CC="i686-pc-mingw32-gcc" \
-  ../configure \
-    --build=$(uname -m)-pc-linux \
-    --host=i686-pc-mingw32 \
-    --prefix="$AUTOBUILD_INSTALL_ROOT/i686-pc-mingw32/sys-root/mingw"
-
-  make
-  make install
-
-  #set -o pipefail
-  #make check 2>&1 | tee "$RESULTS"
-
-  if [ -f /usr/bin/rpmbuild ]; then
-    rpmbuild --nodeps \
-       --define "extra_release $EXTRA_RELEASE" \
-       --define "_sourcedir `pwd`" \
-       -ba --clean mingw32-libvirt-sandbox.spec
-  fi
-fi
