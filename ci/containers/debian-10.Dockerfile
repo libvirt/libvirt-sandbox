crobinso/libosinfo

ci: refresh with latest lcitool manifest
This refresh switches the CI for contributors to be triggered by merge
requests. Pushing to a branch in a fork will no longer run CI pipelines,
in order to avoid consuming CI minutes. To regain the original behaviour
contributors can opt-in to a pipeline on push

   git push <remote> -o ci.variable=RUN_PIPELINE=1

This variable can also be set globally on the repository, though this is
not recommended. Upstream repo pushes to branches will run CI.

The use of containers has changed in this update, with only the upstream
repo creating containers, in order to avoid consuming contributors'
limited storage quotas. A fork with existing container images may delete
them. Containers will be rebuilt upstream when pushing commits with CI
changes to the default branch. Any other scenario with CI changes will
simply install build pre-requisite packages in a throaway environment,
using the ci/buildenv/ scripts. These scripts may also be used on a
contributor's local machines.

With pipelines triggered by merge requests, it is also now possible to
workaround the inability of contributors to run pipelines if they have
run out of CI quota. A project member can trigger a pipeline from the
merge request, which will run in context of upstream, however, note
this should only be done after reviewing the code for any malicious
CI changes.

Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

ci/containers/debian-10.Dockerfile
@@ -11,35 +11,35 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     apt-get install -y eatmydata && \
     eatmydata apt-get dist-upgrade -y && \
     eatmydata apt-get install --no-install-recommends -y \
-            ca-certificates \
-            ccache \
-            check \
-            gcc \
-            gettext \
-            git \
-            gtk-doc-tools \
-            hwdata \
-            libgirepository1.0-dev \
-            libglib2.0-dev \
-            libsoup2.4-dev \
-            libxml2-dev \
-            libxml2-utils \
-            libxslt1-dev \
-            locales \
-            make \
-            ninja-build \
-            osinfo-db-tools \
-            pkgconf \
-            python3 \
-            python3-lxml \
-            python3-pip \
-            python3-pytest \
-            python3-requests \
-            python3-setuptools \
-            python3-wheel \
-            valac \
-            wget \
-            xz-utils && \
+                      ca-certificates \
+                      ccache \
+                      check \
+                      gcc \
+                      gettext \
+                      git \
+                      gtk-doc-tools \
+                      hwdata \
+                      libgirepository1.0-dev \
+                      libglib2.0-dev \
+                      libsoup2.4-dev \
+                      libxml2-dev \
+                      libxml2-utils \
+                      libxslt1-dev \
+                      locales \
+                      make \
+                      ninja-build \
+                      osinfo-db-tools \
+                      pkgconf \
+                      python3 \
+                      python3-lxml \
+                      python3-pip \
+                      python3-pytest \
+                      python3-requests \
+                      python3-setuptools \
+                      python3-wheel \
+                      valac \
+                      wget \
+                      xz-utils && \
     eatmydata apt-get autoremove -y && \
     eatmydata apt-get autoclean -y && \
     sed -Ei 's,^# (en_US\.UTF-8 .*)$,\1,' /etc/locale.gen && \
@@ -49,10 +49,10 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
-RUN pip3 install meson==0.56.0
+RUN /usr/bin/pip3 install meson==0.56.0
 
+ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"
 ENV NINJA "/usr/bin/ninja"
 ENV PYTHON "/usr/bin/python3"
-ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"

ci/containers/fedora-rawhide.Dockerfile
@@ -19,35 +19,35 @@ exec "$@"' > /usr/bin/nosync && \
     chmod +x /usr/bin/nosync && \
     nosync dnf distro-sync -y && \
     nosync dnf install -y \
-        ca-certificates \
-        ccache \
-        check-devel \
-        cppi \
-        gcc \
-        gettext \
-        git \
-        glib2-devel \
-        glibc-langpack-en \
-        gobject-introspection-devel \
-        gtk-doc \
-        hwdata \
-        libsoup-devel \
-        libxml2 \
-        libxml2-devel \
-        libxslt-devel \
-        make \
-        meson \
-        ninja-build \
-        osinfo-db-tools \
-        pkgconfig \
-        python3 \
-        python3-lxml \
-        python3-pytest \
-        python3-requests \
-        rpm-build \
-        vala \
-        wget \
-        xz && \
+               ca-certificates \
+               ccache \
+               check-devel \
+               cppi \
+               gcc \
+               gettext \
+               git \
+               glib2-devel \
+               glibc-langpack-en \
+               gobject-introspection-devel \
+               gtk-doc \
+               hwdata \
+               libsoup-devel \
+               libxml2 \
+               libxml2-devel \
+               libxslt-devel \
+               make \
+               meson \
+               ninja-build \
+               osinfo-db-tools \
+               pkgconfig \
+               python3 \
+               python3-lxml \
+               python3-pytest \
+               python3-requests \
+               rpm-build \
+               vala \
+               wget \
+               xz && \
     nosync dnf autoremove -y && \
     nosync dnf clean all -y && \
     rpm -qa | sort > /packages.txt && \
@@ -55,8 +55,8 @@ exec "$@"' > /usr/bin/nosync && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
+ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"
 ENV NINJA "/usr/bin/ninja"
 ENV PYTHON "/usr/bin/python3"
-ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"

ci/containers/ubuntu-2004.Dockerfile
@@ -11,35 +11,35 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     apt-get install -y eatmydata && \
     eatmydata apt-get dist-upgrade -y && \
     eatmydata apt-get install --no-install-recommends -y \
-            ca-certificates \
-            ccache \
-            check \
-            gcc \
-            gettext \
-            git \
-            gtk-doc-tools \
-            hwdata \
-            libgirepository1.0-dev \
-            libglib2.0-dev \
-            libsoup2.4-dev \
-            libxml2-dev \
-            libxml2-utils \
-            libxslt1-dev \
-            locales \
-            make \
-            ninja-build \
-            osinfo-db-tools \
-            pkgconf \
-            python3 \
-            python3-lxml \
-            python3-pip \
-            python3-pytest \
-            python3-requests \
-            python3-setuptools \
-            python3-wheel \
-            valac \
-            wget \
-            xz-utils && \
+                      ca-certificates \
+                      ccache \
+                      check \
+                      gcc \
+                      gettext \
+                      git \
+                      gtk-doc-tools \
+                      hwdata \
+                      libgirepository1.0-dev \
+                      libglib2.0-dev \
+                      libsoup2.4-dev \
+                      libxml2-dev \
+                      libxml2-utils \
+                      libxslt1-dev \
+                      locales \
+                      make \
+                      ninja-build \
+                      osinfo-db-tools \
+                      pkgconf \
+                      python3 \
+                      python3-lxml \
+                      python3-pip \
+                      python3-pytest \
+                      python3-requests \
+                      python3-setuptools \
+                      python3-wheel \
+                      valac \
+                      wget \
+                      xz-utils && \
     eatmydata apt-get autoremove -y && \
     eatmydata apt-get autoclean -y && \
     sed -Ei 's,^# (en_US\.UTF-8 .*)$,\1,' /etc/locale.gen && \
@@ -49,10 +49,10 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
-RUN pip3 install meson==0.56.0
+RUN /usr/bin/pip3 install meson==0.56.0
 
+ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"
 ENV NINJA "/usr/bin/ninja"
 ENV PYTHON "/usr/bin/python3"
-ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"

libvirt/libvirt-sandbox

ci/containers/debian-10.Dockerfile
@@ -11,34 +11,34 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     apt-get install -y eatmydata && \
     eatmydata apt-get dist-upgrade -y && \
     eatmydata apt-get install --no-install-recommends -y \
-            autoconf \
-            automake \
-            autopoint \
-            ca-certificates \
-            ccache \
-            cpp \
-            gcc \
-            gettext \
-            git \
-            gtk-doc-tools \
-            libc-dev-bin \
-            libc6-dev \
-            libcap-ng-dev \
-            libgirepository1.0-dev \
-            libglib2.0-dev \
-            liblzma-dev \
-            libselinux1-dev \
-            libtirpc-dev \
-            libtool \
-            libtool-bin \
-            libvirt-dev \
-            libvirt-glib-1.0-dev \
-            libxml2-dev \
-            locales \
-            make \
-            pkgconf \
-            xz-utils \
-            zlib1g-dev && \
+                      autoconf \
+                      automake \
+                      autopoint \
+                      ca-certificates \
+                      ccache \
+                      cpp \
+                      gcc \
+                      gettext \
+                      git \
+                      gtk-doc-tools \
+                      libc-dev-bin \
+                      libc6-dev \
+                      libcap-ng-dev \
+                      libgirepository1.0-dev \
+                      libglib2.0-dev \
+                      liblzma-dev \
+                      libselinux1-dev \
+                      libtirpc-dev \
+                      libtool \
+                      libtool-bin \
+                      libvirt-dev \
+                      libvirt-glib-1.0-dev \
+                      libxml2-dev \
+                      locales \
+                      make \
+                      pkgconf \
+                      xz-utils \
+                      zlib1g-dev && \
     eatmydata apt-get autoremove -y && \
     eatmydata apt-get autoclean -y && \
     sed -Ei 's,^# (en_US\.UTF-8 .*)$,\1,' /etc/locale.gen && \
@@ -48,6 +48,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
+ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"
-ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"

ci/containers/fedora-rawhide.Dockerfile
@@ -19,36 +19,36 @@ exec "$@"' > /usr/bin/nosync && \
     chmod +x /usr/bin/nosync && \
     nosync dnf distro-sync -y && \
     nosync dnf install -y \
-        autoconf \
-        automake \
-        ca-certificates \
-        ccache \
-        cpp \
-        gcc \
-        gettext \
-        gettext-devel \
-        git \
-        glib2-devel \
-        glibc-langpack-en \
-        glibc-static \
-        gobject-introspection-devel \
-        gtk-doc \
-        libcap-ng-devel \
-        libselinux-devel \
-        libtirpc-devel \
-        libtool \
-        libvirt-devel \
-        libvirt-gobject-devel \
-        libxml2-devel \
-        make \
-        pkgconfig \
-        rpcgen \
-        rpm-build \
-        xz \
-        xz-devel \
-        xz-static \
-        zlib-devel \
-        zlib-static && \
+               autoconf \
+               automake \
+               ca-certificates \
+               ccache \
+               cpp \
+               gcc \
+               gettext \
+               gettext-devel \
+               git \
+               glib2-devel \
+               glibc-langpack-en \
+               glibc-static \
+               gobject-introspection-devel \
+               gtk-doc \
+               libcap-ng-devel \
+               libselinux-devel \
+               libtirpc-devel \
+               libtool \
+               libvirt-devel \
+               libvirt-gobject-devel \
+               libxml2-devel \
+               make \
+               pkgconfig \
+               rpcgen \
+               rpm-build \
+               xz \
+               xz-devel \
+               xz-static \
+               zlib-devel \
+               zlib-static && \
     nosync dnf autoremove -y && \
     nosync dnf clean all -y && \
     rpm -qa | sort > /packages.txt && \
@@ -56,6 +56,6 @@ exec "$@"' > /usr/bin/nosync && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
+ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"
-ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"

ci/containers/ubuntu-2004.Dockerfile
@@ -11,45 +11,34 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     apt-get install -y eatmydata && \
     eatmydata apt-get dist-upgrade -y && \
     eatmydata apt-get install --no-install-recommends -y \
-            autoconf \
-            automake \
-            autopoint \
-            ca-certificates \
-            ccache \
-            cpp \
-            gcc \
-            gettext \
-            git \
-            gtk-doc-tools \
-            libc-dev-bin \
-            libc6-dev \
-            libcap-ng-dev \
-            libgirepository1.0-dev \
-            libglib2.0-dev \
-            libgnutls28-dev \
-            liblzma-dev \
-            libnl-3-dev \
-            libnl-route-3-dev \
-            libselinux1-dev \
-            libtirpc-dev \
-            libtool \
-            libtool-bin \
-            libxml2-dev \
-            libxml2-utils \
-            locales \
-            make \
-            ninja-build \
-            perl-base \
-            pkgconf \
-            python3 \
-            python3-docutils \
-            python3-pip \
-            python3-setuptools \
-            python3-wheel \
-            valac \
-            xsltproc \
-            xz-utils \
-            zlib1g-dev && \
+                      autoconf \
+                      automake \
+                      autopoint \
+                      ca-certificates \
+                      ccache \
+                      cpp \
+                      gcc \
+                      gettext \
+                      git \
+                      gtk-doc-tools \
+                      libc-dev-bin \
+                      libc6-dev \
+                      libcap-ng-dev \
+                      libgirepository1.0-dev \
+                      libglib2.0-dev \
+                      liblzma-dev \
+                      libselinux1-dev \
+                      libtirpc-dev \
+                      libtool \
+                      libtool-bin \
+                      libvirt-dev \
+                      libvirt-glib-1.0-dev \
+                      libxml2-dev \
+                      locales \
+                      make \
+                      pkgconf \
+                      xz-utils \
+                      zlib1g-dev && \
     eatmydata apt-get autoremove -y && \
     eatmydata apt-get autoclean -y && \
     sed -Ei 's,^# (en_US\.UTF-8 .*)$,\1,' /etc/locale.gen && \
@@ -59,10 +48,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
-RUN pip3 install meson==0.56.0
-
+ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"
-ENV NINJA "/usr/bin/ninja"
-ENV PYTHON "/usr/bin/python3"
-ENV CCACHE_WRAPPERSDIR "/usr/libexec/ccache-wrappers"